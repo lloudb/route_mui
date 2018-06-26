124c034e197587bf6e0980e566673f3885cecf79
lloudb <lloudb@gmail.com>
2018-06-26 03:01:04 +0300

    Add files via upload

---
+++ src/Map.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Map = () => (
+    <div>
+        <h1>Карта</h1>
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Adfc714b647f0b5ebdcf3f36242f14043bafb0ba9b9b64260a862f5f3e3c44b58&amp;source=constructor" width="500" height="400" frameborder="0"></iframe>
+    </div>
+    );
+    export default Map;
